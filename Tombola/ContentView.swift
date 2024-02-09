4bec597d49e7a7fa99db74d14b36bc142f390db6
hoopzsz <83385539+hoopzsz@users.noreply.github.com>
2024-02-09 02:15:24 -0500

    Initial Commit

---
+++ Tombola/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tombola
+//
+//  Created by Daniel Hooper on 2024-02-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tombola/TombolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TombolaApp.swift
+//  Tombola
+//
+//  Created by Daniel Hooper on 2024-02-09.
+//
+
+import SwiftUI
+
+@main
+struct TombolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
